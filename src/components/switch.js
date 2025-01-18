c2de4468e9ee527a1af97052ad223a1013bd3d5e
yamoo9 <yamoo9@naver.com>
2025-01-18 20:28:27 +0900

    [구현] Switch 컴포넌트
    - 테마 스위치 데모 추가

---
+++ src/components/switch.js
@@ -0,0 +1,82 @@
+class Switch {
+  #element;
+  #parentElement;
+  #switchWrapper;
+  #switchElement;
+
+  constructor(element) {
+    this.#element = element;
+    this.#parentElement = this.#element.parentElement;
+    this.label = this.#element.dataset.label;
+    this.id = crypto.randomUUID();
+    this.#render();
+  }
+
+  get isChecked() {
+    return this.checked === 'true';
+  }
+
+  get checked() {
+    return this.#switchElement.getAttribute('aria-checked');
+  }
+
+  set checked(newValue) {
+    return this.#switchElement.setAttribute('aria-checked', newValue);
+  }
+
+  onUpdate(callback) {
+    this.#updateCallbacks.push(callback);
+  }
+
+  #updateCallbacks = [];
+
+  #render() {
+    this.#switchWrapper = this.#parseElement();
+    this.#switchElement = this.#switchWrapper.querySelector('[role="switch"]')
+    this.#parentElement.replaceChild(this.#switchWrapper, this.#element);
+    this.#bind();
+  }
+
+  #bind() {
+    const wraprerElement = this.#switchWrapper;
+    const labelElement = wraprerElement.querySelector('.switch--label');
+    const switchElement = labelElement.nextElementSibling;
+
+    const handleToggle = () => {
+      this.checked = !this.isChecked;
+      this.#updateCallbacks.forEach(callback => callback(this.isChecked));
+    };
+
+    wraprerElement.addEventListener('click', handleToggle);
+  }
+
+  #parseElement() {
+    const { id, label } = this;
+
+    const tempElement = document.createElement('div');
+    tempElement.insertAdjacentHTML(
+      'beforeend',
+      /* html */ `
+      <div role="group" class="switch--wrapper">
+        <span id="${id}" class="switch--label">${label}</span>
+        <div
+          class="switch"
+          role="switch"
+          tabindex="0"
+          aria-checked="false"
+          aria-labelledby="${id}"
+        >
+          <span class="switch--knob" />
+        </div>
+      </div>
+    `
+    );
+
+    return tempElement.firstElementChild;
+  }
+}
+
+export function createSwitch(selector) {
+  const switchElements = Array.from(document.querySelectorAll(selector));
+  return switchElements.map((element) => new Switch(element));
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './styles/main.css';
+import { createSwitch } from './components/switch.js';
+
+const [firstSwitch] = createSwitch('.switch');
+
+firstSwitch.onUpdate((isChecked) => {
+  const { body } = document;
+  if (isChecked) {
+    body.style.cssText = `
+      color: #fefefe;
+      background-color: #1e1e1e;
+    `;
+  } else {
+    body.style.cssText = `
+      background-color: #fefefe;
+      color: #1e1e1e;
+    `;
+  }
+});
