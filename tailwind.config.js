ab3f8cac5eec832521154c29b70df94ac72e310b
yamoo9 <yamoo9@naver.com>
2025-01-19 13:40:56 +0900

    [구성] Tailwind CSS
    - postcss-import
    - components/js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './pages/**/*.html'],
+  content: ['./index.html', './pages/**/*.html', './src/components/**/*.js'],
   theme: {
     extend: {
       colors: {
